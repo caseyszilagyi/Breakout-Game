3422c6280523a33308ae2cf97fa05e44a2227cd7
Casey Szilagyi <crs79@duke.edu>
2021-01-25 11:43:09 -0500

    Added the initialization of a ball to test out the game loop and to make sure animation was functioning properly

--- src/breakout/BreakoutGame.java
+++ src/breakout/BreakoutGame.java
@@ -18,11 +18,50 @@ import static javafx.animation.Animation.Status.STOPPED;
 
 public class BreakoutGame extends Game{
 
+    private final int GAME_HEIGHT = 800;
+    private final int GAME_WIDTH = 1000;
+
+    //Ball Initialization
+    private final int BALL_RADIUS = 10;
+    private final int BALL_XINITIAL = 500;
+    private final int BALL_YINITIAL = 400;
+    private final int BALL_XVELOCITY = 0;
+    private final int BALL_YVELOCITY = 5;
+
+
     public BreakoutGame(int fps, String title){
         super(fps, title);
     }
 
-    public void initialize(final Stage primaryStage){
+    public void start(final Stage primaryStage){
+        primaryStage.setTitle(getWindowTitle());
+
+        setSceneNodes(new Group());
+        setGameSurface(new Scene(getSceneNodes(), GAME_WIDTH, GAME_HEIGHT));
+        primaryStage.setScene(getGameSurface());
+
+        makeBall(BALL_XINITIAL, BALL_YINITIAL, BALL_RADIUS, BALL_XVELOCITY, BALL_YVELOCITY);
 
     }
+
+    @Override
+    protected void handleUpdate(Sprite sprite){
+        sprite.update();
+    }
+
+    protected boolean handleCollision(Sprite spriteA, Sprite spriteB){
+        return false;
+    }
+
+
+
+    public void makeBall(int xPos, int yPos, int radius, int xVel, int yVel){
+        Ball ball = new Ball(xPos, yPos, radius, xVel, yVel);
+        // add to all sprite objects in play
+        getSpriteManager().addSprites(ball);
+        // add sprite's
+        getSceneNodes().getChildren().add(0, ball.node);
+    }
+
+
 }
