fe3fd28eb14bc3b17dc97c1085cbeb4054985966
Casey Szilagyi <crs79@duke.edu>
2021-01-25 12:18:35 -0500

    Added javadoc comments (forgot before)

--- src/breakout/Ball.java
+++ src/breakout/Ball.java
@@ -4,6 +4,18 @@ import javafx.scene.shape.Circle;
 
 public class Ball extends Sprite{
 
+    private int xDirection = 1;
+    private int yDirection = 1;
+    /**
+     * The ball class represents the ball that is used to play the breakout game. Although one ball
+     * is all that is initially instantiated, it is possible to instantiate more if needed for
+     * a powerup
+     * @param xPos Starting x position of the ball
+     * @param yPos Starting y position of the ball
+     * @param radius Starting radius of the ball
+     * @param xVel Starting x velocity of the ball
+     * @param yVel Starting y velocity of the ball
+     */
     public Ball(int xPos, int yPos, int radius, int xVel, int yVel){
         Circle ball = new Circle(xPos, yPos, radius);
         vX = xVel;
@@ -11,12 +23,18 @@ public class Ball extends Sprite{
         node = ball;
     }
 
+    /**
+     * Updates the position of the ball. Also checks for collision with the edge of the playing field
+     */
     @Override
     public void update(){
-        node.setTranslateX(node.getTranslateX() + vX);
-        node.setTranslateY(node.getTranslateY() + vY);
+        node.setTranslateX(node.getTranslateX() + vX * xDirection);
+        node.setTranslateY(node.getTranslateY() + vY * yDirection);
 
         //Need to set wall bouncing
+        if(node.getTranslateX() > GAME_HEIGHT){
+
+        }
     }
 
     //Need to add separate collide methods for the paddle and the bricks.
