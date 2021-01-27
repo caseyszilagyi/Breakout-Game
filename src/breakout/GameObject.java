def044c3118ad07990dfeca07bdcffc50d0141c4
Casey Szilagyi <crs79@duke.edu>
2021-01-26 19:08:48 -0500

    This is the most basic object class. It has a few basic properties but most are determined in inheritance

---
+++ src/breakout/GameObject.java
@@ -0,0 +1,39 @@
+package breakout;
+
+import java.util.ArrayList;
+import java.util.List;
+import javafx.scene.Node;
+
+/**
+ * Represents an object to be displayed
+ * Update and collide methods are called at every keyframe
+ * @author Casey Szilagyi
+ */
+public abstract class GameObject {
+    /** Current display node */
+    public Node node;
+
+    /** Velocity vector x direction */
+    public double XVelocity = 0;
+    /** Velocity vector y direction */
+    public double YVelocity = 0;
+
+    /** Does this sprite still exist */
+    public boolean isDead = false;
+
+    /**
+     * Updates the object. This includes things such as reversing direction and moving a certain distance
+     * as determined by the velocity
+     */
+    public abstract void update();
+
+    /**
+     * The collide method simply uses intersects, which bounds each node into a rectangle and checks to see if
+     * they intersect.
+     * @param other - The other object.
+     * @return Whether or not they collided
+     */
+    public boolean collide(Sprite other){
+        return node.getBoundsInParent().intersects(other.node.getBoundsInParent());
+    }
+}
