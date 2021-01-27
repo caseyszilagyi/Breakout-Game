0e14169318da028ee7275fa060791a9b40b6e963
Casey Szilagyi <crs79@duke.edu>
2021-01-26 21:15:23 -0500

    Extends the regular game object for rectangles

---
+++ src/breakout/RectangleGameObject.java
@@ -0,0 +1,39 @@
+package breakout;
+
+import javafx.scene.shape.Rectangle;
+
+public abstract class RectangleGameObject extends GameObject{
+
+    /**
+     * The rectangle class represents any sprite that is rectangular, or has a rectuangular hitbox.
+     * Also includes the borders.
+     * @param startX Starting x position of the rectangle
+     * @param startY Starting y position of the rectangle
+     * @param width Starting width of the rectangle
+     * @param height Starting height of the rectangle
+     */
+    public RectangleGameObject(int startX, int startY, int width, int height, int xVel, int yVel){
+        Rectangle rectangle = new Rectangle(startX, startY, width, height);
+        XVelocity = xVel;
+        YVelocity = yVel;
+        node = rectangle;
+    }
+
+    /**
+     * The most basic form of the update method simply changes the positioning of the rectangle
+     */
+    public void update(){
+        node.setTranslateX(node.getTranslateX() + XVelocity);
+        node.setTranslateY(node.getTranslateY() + YVelocity);
+    }
+
+    /**
+     * Did this sprite collide into the other sprite?
+     * @param other - The other sprite.
+     * @return True if it did
+     */
+    public boolean collide(GameObject other){
+        return node.getBoundsInParent().intersects(other.node.getBoundsInParent());
+    }
+
+}
