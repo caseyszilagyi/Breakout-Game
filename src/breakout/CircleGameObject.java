e62ee21bee6ad970da9c363e87e649d020182215
Casey Szilagyi <crs79@duke.edu>
2021-01-26 21:15:42 -0500

    Exttends the regular game object for circles

---
+++ src/breakout/CircleGameObject.java
@@ -0,0 +1,41 @@
+package breakout;
+
+import javafx.scene.shape.Circle;
+
+public abstract class CircleGameObject extends GameObject{
+
+    /**
+     * The circle class represents any sprite that is circular, or has a circular hitbox.
+     * @param xPos Starting x position of the circle
+     * @param yPos Starting y position of the circle
+     * @param radius Starting radius of the circle
+     * @param xVel Starting x velocity of the circle
+     * @param yVel Starting y velocity of the circle
+     */
+    public CircleGameObject(int xPos, int yPos, int radius, int xVel, int yVel){
+        Circle circle = new Circle(xPos, yPos, radius);
+        XVelocity = xVel;
+        YVelocity = yVel;
+        node = circle;
+    }
+
+    /**
+     * Updates the position of the circle. This is the most basic movement, anything beyond this is implemented
+     * in the subclasses.
+     */
+    @Override
+    public void update(){
+        node.setTranslateX(node.getTranslateX() + XVelocity);
+        node.setTranslateY(node.getTranslateY() + YVelocity);
+    }
+
+    /**
+     * Simply returns the super call of collide, which uses the intersect method and the bounds of the two nodes
+     * @param other - The other sprite.
+     * @return True if it did
+     */
+    public boolean collide(GameObject other){
+        return super.collide(other);
+    }
+
+}
