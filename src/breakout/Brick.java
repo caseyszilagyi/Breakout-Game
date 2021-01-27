f06bb7226bb50cbaf1564a8c34c9b74fedc0ba04
Casey Szilagyi <crs79@duke.edu>
2021-01-27 14:30:48 -0500

    Basic brick class. Abstract so it can't be implemented, various brick types will be subclasses

---
+++ src/breakout/Brick.java
@@ -0,0 +1,33 @@
+package breakout;
+
+public abstract class Brick extends RectangleGameObject{
+
+    /**
+     * The brick class is how all of the bricks in the game are made. The class is abstract so that
+     * any specific type of brick must be made through the use of a subclass
+     * @param startX Starting x position of the rectangle
+     * @param startY Starting y position of the rectangle
+     * @param width Starting width of the rectangle
+     * @param height Starting height of the rectangle
+     */
+    public Brick(int startX, int startY, int width, int height, int xVel, int yVel){
+        super(startX, startY, width, height, xVel, yVel);
+    }
+
+    /**
+     * The most basic form of the update method simply changes the positioning of the rectangle
+     */
+    public void update(){
+        super.update();
+    }
+
+    /**
+     * Did this GameObject collide into the other one
+     * @param other - The other object.
+     * @return True if it did
+     */
+    public boolean collide(GameObject other){
+        return super.collide(other);
+    }
+
+}
