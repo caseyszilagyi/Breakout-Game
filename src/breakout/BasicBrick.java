d9cdec7d58a50795a4bdff5a1bbdffb7203b8376
Casey Szilagyi <crs79@duke.edu>
2021-01-27 22:20:57 -0500

    changed the constructor

--- src/breakout/BasicBrick.java
+++ src/breakout/BasicBrick.java
@@ -4,12 +4,13 @@ public class BasicBrick extends Brick{
 
     /**
      * The basic brick class is the most simple type of brick. It takes one hit to destroy, and doesn't
-     * drop any power ups, and also doesn't move
+     * drop any power ups.
      * @param startX Starting x position of the rectangle
      * @param startY Starting y position of the rectangle
      * @param width Starting width of the rectangle
      * @param height Starting height of the rectangle
-     *
+     * @param xVel Starting x Velocity of the rectangle
+     * @param yVel Starting y Velocity of the rectangle
      */
     public BasicBrick(int startX, int startY, int width, int height){
         super(startX, startY, width, height, 0, 0);
