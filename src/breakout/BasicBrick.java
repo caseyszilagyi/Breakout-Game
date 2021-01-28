2e11b0283df50604c0819fc45b09a1b9f75b709f
Casey Szilagyi <crs79@duke.edu>
2021-01-27 22:18:59 -0500

    comments

--- src/breakout/BasicBrick.java
+++ src/breakout/BasicBrick.java
@@ -4,16 +4,15 @@ public class BasicBrick extends Brick{
 
     /**
      * The basic brick class is the most simple type of brick. It takes one hit to destroy, and doesn't
-     * drop any power ups.
+     * drop any power ups, and also doesn't move
      * @param startX Starting x position of the rectangle
      * @param startY Starting y position of the rectangle
      * @param width Starting width of the rectangle
      * @param height Starting height of the rectangle
-     * @param xVel Starting x Velocity of the rectangle
-     * @param yVel Starting y Velocity of the rectangle
+     *
      */
-    public BasicBrick(int startX, int startY, int width, int height, int xVel, int yVel){
-        super(startX, startY, width, height, xVel, yVel);
+    public BasicBrick(int startX, int startY, int width, int height){
+        super(startX, startY, width, height, 0, 0);
     }
 
     /**
